197ece7cfb88959749b32032f566a6c16d8d3875
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-07-14 14:54:15 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ProjectList from "./components/ProjectList.vue";
+
+export default {
+  components: {
+    ProjectList,
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-center p-3">Boolpress</h1>
+  <div class="container">
+    <ProjectList />
+  </div>
+</template>
+
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+</style>
